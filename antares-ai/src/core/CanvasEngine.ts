c0a4fe8f035242ac5ae5b22e0982265aa3c68cd1
zzztzzzt <180073683+zzztzzzt@users.noreply.github.com>
2026-06-29 12:52:58 +0800

    [style] make TS code more readable

--- antares-ai/src/core/CanvasEngine.ts
+++ antares-ai/src/core/CanvasEngine.ts
@@ -1,36 +1,29 @@
 export class CanvasEngine {
-  private canvas: HTMLCanvasElement
-  private ctx: CanvasRenderingContext2D
+  private readonly canvas: HTMLCanvasElement;
+  private readonly ctx: CanvasRenderingContext2D;
 
   constructor(canvas: HTMLCanvasElement) {
-    const ctx = canvas.getContext("2d")
+    const ctx = canvas.getContext("2d");
 
     if (!ctx) {
-      throw new Error("Cannot get 2D context")
+      throw new Error("Cannot get 2D context");
     }
 
-    this.canvas = canvas
-    this.ctx = ctx
+    this.canvas = canvas;
+    this.ctx = ctx;
   }
 
-  drawImage(image: HTMLImageElement) {
-    this.canvas.width = image.width
-    this.canvas.height = image.height
-
-    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
-    this.ctx.drawImage(image, 0, 0)
+  drawImage(image: HTMLImageElement): void {
+    this.canvas.width = image.width;
+    this.canvas.height = image.height;
+    this.ctx.drawImage(image, 0, 0);
   }
 
   getImageData(): ImageData {
-    return this.ctx.getImageData(
-      0,
-      0,
-      this.canvas.width,
-      this.canvas.height
-    )
+    return this.ctx.getImageData(0, 0, this.canvas.width, this.canvas.height);
   }
 
-  putImageData(imageData: ImageData) {
-    this.ctx.putImageData(imageData, 0, 0)
+  putImageData(imageData: ImageData): void {
+    this.ctx.putImageData(imageData, 0, 0);
   }
 }
--- antares-ai/src/filters/VibranceFilter.ts
+++ antares-ai/src/filters/VibranceFilter.ts
@@ -1,5 +1,4 @@
 import type { ImageFilter } from "./ImageFilter";
-
 import { transformOKLCH } from "../utils/color/";
 
 export class VibranceFilter implements ImageFilter {
@@ -7,37 +6,17 @@ export class VibranceFilter implements ImageFilter {
 
   apply(imageData: ImageData): ImageData {
     const data = imageData.data;
-
     const strength = this.amount / 100;
 
     const MAX_CHROMA = 0.4;
-
     const SCALE = 0.08;
 
     for (let i = 0; i < data.length; i += 4) {
       const rgb = transformOKLCH(
-        {
-          r: data[i],
-          g: data[i + 1],
-          b: data[i + 2],
-        },
+        { r: data[i], g: data[i + 1], b: data[i + 2] },
         (lch) => {
-          const boost =
-            1 -
-            Math.min(
-              lch.c / MAX_CHROMA,
-              1
-            );
-
-          lch.c +=
-            strength *
-            boost *
-            SCALE;
-
-          lch.c = Math.max(
-            0,
-            lch.c
-          );
+          const boost = 1 - Math.min(lch.c / MAX_CHROMA, 1);
+          lch.c = Math.max(0, lch.c + strength * boost * SCALE);
         }
       );
 
